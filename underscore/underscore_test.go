f5c28d4ad3b568a5631da6dd19a2f1c89f283b75
Michael Miranda <m.miranda1219@gmail.com>
2020-05-30 12:47:49 -0400

    refactor underscore test

--- underscore/underscore_test.go
+++ underscore/underscore_test.go
@@ -1,27 +1,26 @@
 package underscore
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestCamel(t *testing.T) {
-	type args struct {
-		str string
-	}
-
 	tests := []struct {
-		name string
-		args args
+		arg  string
 		want string
 	}{
-		{"simple", args{"thisIsACamelCaseString"}, "this_is_a_camel_case_string"},
-		{"spaces", args{"with a space"}, "with a space"},
-		{"ends with capital", args{"endsWithA"}, "ends_with_a"},
+		{"thisIsACamelCaseString", "this_is_a_camel_case_string"},
+		{"with a space", "with a space"},
+		{"endsWithA", "ends_with_a"},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Camel(tt.args.str); got != tt.want {
-				t.Errorf("Camel() = %v, want %v", got, tt.want)
+	for _, tc := range tests {
+		t.Run(tc.arg, func(t *testing.T) {
+			if got := Camel(tc.arg); got != tc.want {
+				t.Fatalf("Camel() = %v, want %v", got, tc.want)
 			}
+			fmt.Println("this won't print upon failure")
 		})
 	}
 }
